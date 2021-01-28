6902e8d915a10079d42b37805eea3b04dfc07db3
Skitovich <Zloycvetok@gmail.com>
2021-01-28 14:42:25 +0300

    should be done

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -2,23 +2,36 @@ package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.element;
+import static ru.netology.web.data.DataHelper.getCardsInfo;
 
 public class DashboardPage {
-    private final SelenideElement secondCardDeposit = element("div[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button__text");
-    private final SelenideElement firstCardDeposit = element("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button__text");
-
+    private final SelenideElement fillSecondCardDeposit =
+            element("div[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d'] .button__text");
+    private final SelenideElement fillFirstCardDeposit =
+            element ("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0'] .button__text");
+    private final SelenideElement checkFirstCardDeposit =
+            element("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']");
+    private final SelenideElement checkSecondCardDeposit =
+            element("div[data-test-id='0f3f5c2a-249e-4c3d-8287-09f7a039391d']");
 
     public DashboardPage() {
         SelenideElement heading = $("[data-test-id=dashboard]");
         heading.shouldBe(visible);
+
     }
 
     public MoneyTransferCardsPage depositMoneyToCard(int enterLastNumOfCard) {
-        if (enterLastNumOfCard == 1) firstCardDeposit.click();
-        else secondCardDeposit.click();
+        if (enterLastNumOfCard == 1) fillFirstCardDeposit.click();
+        else fillSecondCardDeposit.click();
         return new MoneyTransferCardsPage();
+    }
+
+    public void cardBalanceCheck () {
+      checkFirstCardDeposit.shouldHave(text(String.valueOf(getCardsInfo().getFirstCardDeposit())));
+        checkSecondCardDeposit.shouldHave(text(String.valueOf(getCardsInfo().getSecondCardDeposit())));
     }
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -3,29 +3,28 @@ package ru.netology.web.test;
 import com.codeborne.selenide.Configuration;
 import lombok.val;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
-
 import ru.netology.web.page.LoginPageV1;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.element;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.web.data.DataHelper.getCardsInfo;
 
 
 class MoneyTransferTest {
+
 
     @BeforeAll
     static void setUp() {
         Configuration.browser = "opera";
         Configuration.startMaximized = true;
-
+    }
+    @BeforeEach
+    void openSetUp () {
+        open("http://localhost:9999");
     }
 
     @Test
     void shouldFillFirstCardSuccessfully() {
-        open("http://localhost:9999");
         val loginPage = new LoginPageV1();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
@@ -33,13 +32,11 @@ class MoneyTransferTest {
         val dashboardPage = verificationPage.validVerify(verificationCode);
         val cardsPage = dashboardPage.depositMoneyToCard(1);
         cardsPage.fillFirstCard(2000);
-        element("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").
-                shouldHave(text(String.valueOf(getCardsInfo().getFirstCardDeposit())));
+        dashboardPage.cardBalanceCheck();
     }
 
     @Test
     void shouldFillSecondCardSuccessfully() {
-        open("http://localhost:9999");
         val loginPage = new LoginPageV1();
         val authInfo = DataHelper.getAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
@@ -47,8 +44,7 @@ class MoneyTransferTest {
         val dashboardPage = verificationPage.validVerify(verificationCode);
         val cardsPage = dashboardPage.depositMoneyToCard(2);
         cardsPage.fillSecondCard(5000);
-        element("div[data-test-id='92df3f1c-a033-48e6-8390-206f6b1f56c0']").
-                shouldHave(text(String.valueOf(getCardsInfo().getSecondCardDeposit())));
+        dashboardPage.cardBalanceCheck();
     }
 
 
