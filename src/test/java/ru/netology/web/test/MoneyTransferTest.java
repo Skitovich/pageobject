8a1480a47568a4b63668a5c2076a6f9b4a23aae7
Skitovich <Zloycvetok@gmail.com>
2021-01-28 14:55:28 +0300

    should be done

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -16,7 +16,7 @@ class MoneyTransferTest {
 
     @BeforeAll
     static void setUp() {
-        Configuration.browser = "fire";
+//        Configuration.browser = "firefox";
         Configuration.startMaximized = true;
     }
 
